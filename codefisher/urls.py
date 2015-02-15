745c0d95dd2b199df1826004811f5b5748a79084
codefisher <mail@codefisher.org>
2015-02-15 22:43:09 +0100

    first commit of files

---
+++ codefisher/urls.py
@@ -0,0 +1,86 @@
+from django.conf.urls import patterns, url, include
+from djangopress.forum import urls as forum_urls
+from djangopress.accounts import urls as accounts_urls
+from djangopress.blog import urls as blog_urls
+from djangopress.pages import urls as pages_urls
+from paypal.standard.ipn import urls as paypal_urls
+from djangopress.donate import urls as donate_urls
+from djangopress.contact import urls as contact_urls
+from djangopress.menus import urls as menu_urls
+from djangopress.iptools import urls as iptools_urls
+
+from codefisher_apps.downloads import urls as download_urls
+from codefisher_apps.svn_xslt import urls as svn_urls
+from codefisher_apps.favicon_getter import urls as favicon_getter_urls
+from codefisher_apps.pastelsvg import urls as pastelsvg_urls
+
+from django.http import HttpResponsePermanentRedirect
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+def redirect(request, url):
+    return HttpResponsePermanentRedirect('/' + url)
+
+# django databrowse application
+#from django.contrib import databrowse
+#from django.contrib.auth.decorators import login_required
+
+urlpatterns = patterns('',
+    # the blog system
+    (r'^(?P<blog_slug>[\w\-]+)/blog/', include(blog_urls)),
+    (r'^news/', include(blog_urls), {"blog_slug": "news"}),
+    
+    # the forum system
+    (r'^(?P<forums_slug>[\w\-]+)/forum/', include(forum_urls)),
+    (r'^forum/', include(forum_urls), {"forums_slug": "codefisher"}),
+    
+    # the user accounts system
+    (r'^accounts/', include(accounts_urls)),
+
+    # the cms pages editing tools etc/
+    (r'^pages/', include(pages_urls)),
+
+    (r'^paypal/', include(paypal_urls)),
+    (r'^donate/', include(donate_urls)),
+    (r'^email/', include(contact_urls)),
+    (r'^iptools/', include(iptools_urls)),
+    (r'^(?P<url>.*/)[a-z_-]+\.php$', redirect),
+    
+    (r'^xslt_svn/', include(svn_urls)),
+    (r'^ico/', include(favicon_getter_urls)),
+    (r'^pastel-svg/', include(pastelsvg_urls)),
+    # the Toolbar Buttons section of the site (custom maker etc.)
+    (r'^toolbar_button/request/',
+        include('tbutton_web.tbutton_votes.urls')),
+    (r'^toolbar_button/',
+        include('tbutton_web.tbutton_maker.urls')),
+)
+
+try:
+    from djangopress.core.search import ModelSetSearchForm, ModelSetSearchView, search_view_factory
+
+    urlpatterns += patterns('',
+        # the haystack search
+        url(r'^search/', search_view_factory(
+                view_class=ModelSetSearchView,
+                form_class=ModelSetSearchForm,
+                results_per_page=10,
+                models=["pages.page", "blog.entry"],
+            ), name='haystack-search'),
+    )
+except ImportError:
+    pass
+
+from djangopress.sitemap import sitemap_patterns
+urlpatterns += sitemap_patterns
+
+from local_urls import urlpatterns as locale_urls
+urlpatterns = locale_urls + urlpatterns
+
+from codefisher_apps.downloads.urls import urlpatterns as download_urls
+urlpatterns += download_urls
+
+from djangopress.core.format.models import autodiscover as format_autodiscover
+format_autodiscover()
---
+++ codefisher_site/settings.py
@@ -0,0 +1,170 @@
+import os
+BASE_DIR = os.path.realpath(os.path.dirname(__file__))
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+ADMINS = (
+    ('Michael Buckley', 'support@codefisher.org'),
+)
+MANAGERS = ADMINS
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'UTC' #None
+
+USE_TZ = True
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = False
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'www', 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static_files')
+MEDIA_UPLOAD = MEDIA_ROOT
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+ROOT_URLCONF = 'codefisher.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, '..', 'templates'),
+)
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.sitemaps',
+    'django.contrib.redirects',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    # djangopress
+    'djangopress.core.format',
+    'djangopress.blog',
+    'djangopress.menus',
+    'djangopress.core',
+    'djangopress.accounts',
+    'djangopress.theme',
+    'djangopress.pages',
+    'djangopress.forum',
+    'djangopress.donate',
+    'djangopress.iptools',
+    'paypal.standard',
+    'paypal.standard.ipn',
+    'djangopress.contact',
+)
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+)
+
+
+PAYPAL_RECEIVER_EMAIL = "paypal@codefisher.org"
+PAYPAL_TEST = False
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '..', 'www', 'static'),
+)
+
+KEEP_LOGGED_DURATION = 30*24*60*60 # about one month
+
+X_FRAME_OPTIONS = 'DENY'
+
+ANONYMOUS_USER_GROUP = "Anonymous"
+DEFAULT_USER_GROUP = "New Member"
+
+# the icon sets to use for the buttons
+TBUTTON_ICONS = {
+    "pastel-svg": ("Pastel SVG", "../www/static/images/pastel-svg"),
+}
+TBUTTON_ICONS_SIZES = [
+    "standard", "large", "jumbo",
+]
+TBUTTON_ICON_SET_SIZES = {
+    "pastel-svg": {
+        "standard": ("16", "24"),
+        "large": ("24", "32"),
+        "jumbo": ("32", "48")
+    },
+}
+TBUTTON_DEFAULT_ICONS = "pastel-svg"
+TBUTTON_TAGS_DIR = None
+
+import mozbutton_sdk
+
+MOZ_APP_NAMES = dict((ext_id, name) for name, ext_id, _, _ in
+        sum(mozbutton_sdk.config.settings.config.get("applications_data").values(), ()))
+
+# the default icons to use for the link buttons
+DEFAULT_LINK_ICONS = "../www/static/images/link-icons"
+
+INSTALLED_APPS += (
+    'tbutton_web.tbutton_maker',
+    'tbutton_web.tbutton_votes',
+    'codefisher_apps.downloads',
+    'codefisher_apps.extension_downloads',
+    'codefisher_apps.svn_xslt',
+    'codefisher_apps.favicon_getter',
+    'codefisher_apps.reverseproxy',
+    'codefisher_apps.online_status',
+    'codefisher_apps.site_crawler',
+    'codefisher_apps.pastelsvg',
+    'upvotes',
+    'haystack',
+    'captcha',
+)
+
+SITE_CRAWLER_DICT_LANG = "en"
+SITE_CRAWLER_DICT_PWL = os.path.join(BASE_DIR, '..', 'wordlist.txt')
+SITE_CRAWLER_REMOVE_IDS = ('footer', 'donations')
+
+
+ACCOUNTS_USER_LIMITS = {
+    "avatar": {
+        "size": 50,
+        "file_size": 1024*100,
+        "max_upload_size": 200,
+    },
+    "attachments": {
+        "file_size": 1024*500,
+        "total": 1024*1024*5,
+    },
+    "signature": {
+        "lines": 4,
+        "length": 500,
+        "images": True,
+        "links": True,         
+    }
+}
