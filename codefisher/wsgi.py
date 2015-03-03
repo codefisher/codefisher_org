e86969ddbe1a82fb761dafda102982a8f4843a96
codefisher <mail@codefisher.org>
2015-03-03 17:48:59 +0100

    adding HOME evn variable

--- codefisher/wsgi.py
+++ codefisher/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codefisher_org.codefisher.settings")
 os.environ['PYTHONHASHSEED'] = 'random'
+# this is needed for stuff like the git plugin, it will try to write to /home/root/ otherwise
 os.environ['HOME'] = os.path.join(os.path.realpath(os.path.dirname(__file__)), '..', '..')
 
 from django.core.wsgi import get_wsgi_application
